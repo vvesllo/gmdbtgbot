b103a4b88a4e6c26247584d9cff1348939054b93
WSLL <70818012+vvesllo@users.noreply.github.com>
2026-02-22 23:45:02 +0500

    added minesweeper game

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import os
 import dotenv
 
 from commands import register_bot_commands
+from minesweeper import register_bot_minesweeper_game
 
 def main() -> int:
     dotenv.load_dotenv()
     bot = telebot.TeleBot(os.getenv("TELEGRAM_TOKEN"))
 
+
     register_bot_commands(bot)
+    register_bot_minesweeper_game(bot)
 
     bot.infinity_polling()
 
---
+++ src/minesweeper.py
@@ -0,0 +1,39 @@
+import telebot
+import random
+
+
+def register_bot_minesweeper_game(bot: telebot.TeleBot) -> None:
+    @bot.message_handler(commands=["ms"])
+    def minesweeper_game(message):
+        reply_markup = telebot.types.InlineKeyboardMarkup()
+
+        values = ['o'] * 6 + ['x'] * 3
+        random.shuffle(values)
+
+        for position_y in range(3):
+            inline_buttons = []
+            for position_x in range(3):
+                inline_buttons.append(
+                    telebot.types.InlineKeyboardButton(
+                        text='?', 
+                        callback_data=f'ms:{values[position_x + position_y]}'
+                        )
+                    )
+            reply_markup.add(*inline_buttons)
+
+        bot.reply_to(
+            message,
+            'Choise',
+            reply_markup=reply_markup
+        )
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def minesweeper_game_callback_query(call):
+        data = call.data.split(':')
+        if data[0] == 'ms':
+            value = data[1]
+
+            bot.edit_message_reply_markup(
+                call.chat.id,
+                call.message.id
+            )
