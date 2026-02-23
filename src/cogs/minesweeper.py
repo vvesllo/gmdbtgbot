4e9a8a75538a3298273efc0a6b37b1f4e512a732
LoaDino <74372772+LoaDino@users.noreply.github.com>
2026-02-23 23:46:29 +0300

    miner!!

--- src/cogs/minesweeper.py
+++ src/cogs/minesweeper.py
@@ -3,36 +3,85 @@ from cogs import bot
 from telebot import types
 import random
 
-@bot.message_handler(commands=["ms"])
-async def minesweeper_game(message):
-    reply_markup = types.InlineKeyboardMarkup()
+games = {}
 
-    values = ['o'] * 6 + ['x'] * 3
-    random.shuffle(values)
+async def create_land(new=False, game_id = None,  opened_full=False):
+    if new:
+        game_id = str(random.randint(10000, 99999))
+        values = ['💎'] * 6 + ['💣'] * 3
+        random.shuffle(values)
+        values_matrix = [values[0:3], values[3:6], values[6:9]]
+        opened = []
+        games[game_id] = [values_matrix, opened]
+
+    values_matrix = games[game_id][0]
+    opened = games[game_id][1]
+
+    reply_markup = types.InlineKeyboardMarkup()
 
     for position_y in range(3):
         inline_buttons = []
         for position_x in range(3):
+            position = str(position_x)+str(position_y)
+            if position not in opened:
+                text = '?'
+            else:
+                text = values_matrix[position_x][position_y]
+            style = None
+            if text == "💣":
+                style = "danger"
+            if text == "💎":
+                style = "success"
+            
+            if opened_full:
+                text = values_matrix[position_x][position_y]
+            
+            callback_data = f'ms:{game_id}:{values_matrix[position_x][position_y]}:{position_x}:{position_y}'
+            if opened_full:
+                callback_data = "ms:ignore"
+
             inline_buttons.append(
                 types.InlineKeyboardButton(
-                    text='?', 
-                    callback_data=f'ms:{values[position_x + position_y]}'
+                    text=text, 
+                    callback_data=callback_data,
+                    style=style
                     )
                 )
         reply_markup.add(*inline_buttons)
+    
+    return reply_markup
 
+@bot.message_handler(commands=["ms"])
+async def minesweeper_game(message):
     await bot.reply_to(
         message,
-        'Choise',
-        reply_markup=reply_markup
+        '===========Choice============',
+        reply_markup=await create_land(new = True)
     )
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("ms"))
 async def minesweeper_game_callback_query(call):
     data = call.data.split(":")
-    value = data[1]
+    game_id = data[1]
+    if game_id == "ignore":
+        await bot.answer_callback_query(
+            callback_query_id=call.id,
+            text="game over!",
+            show_alert=True
+        )
+        return
+    
+    value = data[2]
+    position_x = data[3]
+    position_y = data[4]
+    opened = games[game_id][1]
 
-    bot.edit_message_reply_markup(
-        call.chat.id,
-        call.message.id
-    )
+    if position_x+position_y not in opened:
+        opened.append(position_x+position_y)
+        games[game_id][1] = opened
+
+        await bot.edit_message_reply_markup(
+            chat_id=call.message.chat.id,
+            message_id=call.message.id,
+            reply_markup=await create_land(game_id=game_id, opened_full=value == '💣')
+        )
